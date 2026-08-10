bdde10c5bc0eb41e673ec4723679cdd98c28632c
Rishabh Choudhary <rishabhchoudhary140126@gmail.com>
2026-08-10 15:34:47 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.61%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
+double helper(double x, long long n){
+        double ans = 1;
+        if(n>=0){
+            while(n>0){
+                int lastbit = n & 1;
+                n = n>>1;
+                if(lastbit == 1){
+                    ans = ans*x;
+                    x = x*x;
+                }
+                else x = x*x;
+            }
+        }
+        else{
+            n = n * -1;
+            
+            while(n>0){
+                int lastbit = n & 1;
+                n = n>>1;
+                if(lastbit == 1){
+                    ans = ans/x;
+                    x = x*x;
+                }
+                else x = x*x;
+            }
+            
+        }
+        return ans;
+}
     double myPow(double x, int n) {
-        double ans= 1.0;
-        long long N = n;
-        if(N<0){
-            N = N* -1;
-            x = 1/x;
-        }
-        while(N>0){
-            if(N%2 == 1) ans = ans*x;
-            x*=x;
-            N/=2;
-        }
+        double ans = helper(x, n);
         return ans;
     }
 };
